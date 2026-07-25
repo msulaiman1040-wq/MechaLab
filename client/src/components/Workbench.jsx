7b85ce62225b1d1edccaa383fca33aaa251f41cc
msulaiman1040-wq <msulaiman1040@gmail.com>
2026-07-25 03:18:22 +0100

    fun

--- client/src/components/Workbench.jsx
+++ client/src/components/Workbench.jsx
@@ -1,5 +1,5 @@
 import { playInstallSound } from "../managers/SoundManager";
-import { useWorkshop } from "../Context/WorkShopContext";
+import { useWorkshop } from "../Context/WorkshopContext";
 import { useEffect, useState, useRef } from "react";
 import BuildManager from "../managers/BuildManager";
 import InstallManager from "../managers/InstallManager";
